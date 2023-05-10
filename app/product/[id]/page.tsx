b405679da4100bd91fa66968a594d64ec7e7b276
paul-cute <paulcuteflorin@protonmail.com>
2023-05-10 23:43:38 +0300

    product id page style

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -9,13 +9,24 @@ interface Props {
 
 async function ProductPage({params: {id}}: Props) {
 
-  const res = await fetch(`https://fakestore.com/products/${id}`);
+  const res = await fetch(`https://fakestoreapi.com/products/${id}`);
 
   const product:Product = await res.json();
 
   return (
-    <div>
+    <div className='max-w-5xl mx-auto flex flex-col md:flex-row items-center gap-8 px-4 mt-48 pb-10'>
       <ProductImage product={product}/>
+
+      <div className='divide-y'>
+        <div className='space-y-2 pb-8'>
+          <h1 className='text-2xñ md:text-4xl font-bold'>{product.title}</h1>
+          <h2 className='text-gray-500 font-bold text-xl md:text-3xl'>€{product.price}</h2>
+        </div>
+
+        <div className='pt-8'>
+          <p className='text-xs md:text-sm'>{product.description}</p>
+        </div>
+      </div>
     </div>
   )
 }
